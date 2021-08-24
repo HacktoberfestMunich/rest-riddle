44d3bf8c0900d223c0938ee56565db8a6b399be4
Tobias Schaffner <tobias.schaffner@mixed-mode.de>
2021-08-24 22:42:14 +0200

    Add first riddles

--- src/main/kotlin/de/pixel/restriddle/challenges/TutorialChallengesController.kt
+++ src/main/kotlin/de/pixel/restriddle/challenges/TutorialChallengesController.kt
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class TutorialChallengesController {
@@ -11,7 +12,34 @@ class TutorialChallengesController {
     @GetMapping("/", "/index")
     fun index(): String = "redirect:index.html"
 
-    @GetMapping("/fun")
+    @GetMapping("/lHjKMVVICUFo355UVqWk")
     @ResponseBody
-    fun `fun`(): ResponseEntity<String> = ResponseEntity.ok("\uD83E\uDD73")
+    fun `lHjKMVVICUFo355UVqWk`(): ResponseEntity<String> = ResponseEntity.ok("<img src=\"goodjob.png\">")
+
+    var tryAgainCounter: Int = 0
+
+    @GetMapping("/O5ZdvcloSZ6J42zZh3vy")
+    @ResponseBody
+    fun `O5ZdvcloSZ6J42zZh3vy`(): ResponseEntity<String> {
+        if (tryAgainCounter < 20) {
+            tryAgainCounter++
+            return ResponseEntity.ok("Try again!")
+        }
+        else
+            return ResponseEntity.ok("<a href=\"KHV8vhIwGcDaBHDS43Em\">This way</a>")        
+    }
+   
+    @GetMapping("/KHV8vhIwGcDaBHDS43Em")
+    @ResponseBody
+    fun `KHV8vhIwGcDaBHDS43Em`(@RequestParam allParams: Map<String,String>): ResponseEntity<String> {
+        if (allParams.isEmpty())
+            return ResponseEntity.ok("Give me more options!")
+        else
+            return ResponseEntity.ok("<a href=\"urCMB0OlZkeU3OXNNFxP\">You crazy bitch!</a>")        
+    }
+
+    @GetMapping("/urCMB0OlZkeU3OXNNFxP")
+    @ResponseBody
+    fun `urCMB0OlZkeU3OXNNFxP`(): ResponseEntity<String> = ResponseEntity.ok("\uD83E\uDD73")
+
 }
