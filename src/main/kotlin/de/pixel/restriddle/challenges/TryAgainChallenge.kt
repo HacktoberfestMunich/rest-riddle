5cfcb3901d69e22151e607e141dcfa716ee988ab
Markus Pöschl <Poeschl@users.noreply.github.com>
2021-10-09 16:42:11 +0200

    :recycle: Add timed reset of try-again challenge

--- src/main/kotlin/de/pixel/restriddle/challenges/TryAgainChallenge.kt
+++ src/main/kotlin/de/pixel/restriddle/challenges/TryAgainChallenge.kt
@@ -1,20 +1,32 @@
 package de.pixel.restriddle.challenges
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.concurrent.timer
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
 
+@ExperimentalTime
 @RestController
 @Qualifier("tryagain")
-class TryAgainChallenge(@Qualifier("imageContainer") nextChallenge: ChallengeController) : ChallengeController(ENTRYPOINT, "Try Again Challenge", nextChallenge) {
+class TryAgainChallenge(@Qualifier("imageContainer") nextChallenge: ChallengeController) :
+    ChallengeController(ENTRYPOINT, "Try Again Challenge", nextChallenge) {
 
     companion object {
+        private val LOGGER = LoggerFactory.getLogger(TryAgainChallenge::class.java)
         const val ENTRYPOINT = "4E4yXR2USj7m9GxL"
         private const val TRY_COUNTS = 20
     }
 
     private var tryAgainCounter = 0
+    private val resetTimer =
+        timer("TryAgainResetTimer", true, 0, Duration.minutes(10).inWholeMilliseconds) {
+            resetCounter()
+        }
+
 
     @GetMapping("/${ENTRYPOINT}")
     fun challenge(): ResponseEntity<String> {
@@ -26,5 +38,10 @@ class TryAgainChallenge(@Qualifier("imageContainer") nextChallenge: ChallengeCon
         } else {
             page.addLink(nextChallenge?.entrypoint.orEmpty(), "Next").build()
         }
+    }
+
+    private fun resetCounter() {
+        LOGGER.debug("Reseting try-again counter")
+        tryAgainCounter = 0
     }
 }
