df567a8a02fc2f7213038e137ff0f7277e98314b
Markus Pöschl <Poeschl@users.noreply.github.com>
2021-10-12 20:59:18 +0200

    :recycle: Remove reset timer on TryAgain

--- src/main/kotlin/de/pixel/restriddle/challenges/TryAgainChallenge.kt
+++ src/main/kotlin/de/pixel/restriddle/challenges/TryAgainChallenge.kt
@@ -1,12 +1,9 @@
 package de.pixel.restriddle.challenges
 
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import kotlin.concurrent.timer
-import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
@@ -16,16 +13,11 @@ class TryAgainChallenge(@Qualifier("imageContainer") nextChallenge: ChallengeCon
     ChallengeController(ENTRYPOINT, "Tree trunk", nextChallenge) {
 
     companion object {
-        private val LOGGER = LoggerFactory.getLogger(TryAgainChallenge::class.java)
         const val ENTRYPOINT = "4E4yXR2USj7m9GxL"
-        private const val TRY_COUNTS = 20
+        private const val TRY_COUNTS = 30
     }
 
     private var tryAgainCounter = 0
-    private val resetTimer =
-        timer("TryAgainResetTimer", true, 0, Duration.minutes(10).inWholeMilliseconds) {
-            resetCounter()
-        }
 
     @GetMapping("/${ENTRYPOINT}")
     fun challenge(): ResponseEntity<String> {
@@ -40,10 +32,5 @@ class TryAgainChallenge(@Qualifier("imageContainer") nextChallenge: ChallengeCon
         } else {
             page.addLink(nextChallenge?.entrypoint.orEmpty(), "Pass the split trunk.").build()
         }
-    }
-
-    private fun resetCounter() {
-        LOGGER.debug("Reseting try-again counter")
-        tryAgainCounter = 0
     }
 }
