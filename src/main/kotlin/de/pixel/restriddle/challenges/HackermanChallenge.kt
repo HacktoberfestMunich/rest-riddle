aaa5e0e3f6a2bbba7e862ba0cc332713b5414263
Markus Pöschl <Poeschl@users.noreply.github.com>
2021-10-12 15:11:45 +0200

    :recycle: Change video to Löwenzahn

--- src/main/kotlin/de/pixel/restriddle/challenges/HackermanChallenge.kt
+++ src/main/kotlin/de/pixel/restriddle/challenges/HackermanChallenge.kt
@@ -12,13 +12,14 @@ import javax.servlet.http.HttpServletResponse
 @RestController
 @Qualifier("hackerman")
 class HackermanChallenge(@Qualifier("deleteme") nextChallenge: ChallengeController) :
-    ChallengeController(ENTRYPOINT, "Time hacking time", nextChallenge) {
+    ChallengeController(ENTRYPOINT, "The way to Bärenstein", nextChallenge) {
 
     companion object {
         const val ENTRYPOINT = "mPSfA4qxXOKNg47rJtnK"
-        private const val HACKERMAN_YOUTUBE = "https://www.youtube.com/watch?v=KEkrWRHCDQU"
-        private const val QUESTION = "What do we need to proceed to make a direct link to the kernel?"
-        private const val ANSWER = "segmentation violation"
+        private const val HACKERMAN_YOUTUBE = "https://www.youtube.com/watch?v=uFs9KQYx8D8"
+        private const val QUESTION = "Which river we need to follow to find the castle of Bärenstein?"
+        private const val ANSWER1 = "Rhine"
+        private const val ANSWER2 = "Rhein"
     }
 
     @GetMapping("/${ENTRYPOINT}")
@@ -40,7 +41,7 @@ class HackermanChallenge(@Qualifier("deleteme") nextChallenge: ChallengeControll
 
     @PostMapping("/${ENTRYPOINT}/post")
     fun accept(@RequestParam(defaultValue = "") input: String, response: HttpServletResponse): ResponseEntity<String> {
-        return if (input.isNotBlank() && input.contains(ANSWER, true)) {
+        return if (input.isNotBlank() && (input.contains(ANSWER1, true) || input.contains(ANSWER2, true))) {
             getPage().addLink(nextChallenge?.entrypoint.orEmpty(), "Continue").build()
         } else {
             response.sendRedirect("/${ENTRYPOINT}")
