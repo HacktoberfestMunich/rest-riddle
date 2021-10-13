92c1fdcba0adf8ea299b0a6b1d8df557987b7666
Markus Pöschl <Poeschl@users.noreply.github.com>
2021-10-13 20:49:49 +0200

    :bug: Fix CookieChallenge

--- src/main/kotlin/de/pixel/restriddle/challenges/AllCookiesChallenge.kt
+++ src/main/kotlin/de/pixel/restriddle/challenges/AllCookiesChallenge.kt
@@ -14,13 +14,13 @@ class AllCookiesChallenge(@Qualifier("userAgent") nextChallenge: ChallengeContro
     companion object {
         const val ENTRYPOINT = "gQbFIbllHK6bjlQ95A22"
         private const val COOKIE_YOUTUBE = "https://www.youtube.com/watch?v=Ye8mB6VsUHw"
-        private const val COOKIE_COUNT = 3
+        private const val COOKIE_COUNT = 5
     }
 
     @GetMapping("/${ENTRYPOINT}")
     fun challenge(servletRequest: HttpServletRequest): ResponseEntity<String> {
         val cookies = servletRequest.cookies
-        return if (cookies != null && cookies.count() > 5) {
+        return if (cookies != null && cookies.count() > COOKIE_COUNT) {
             getPage().addElement("Well done!").addLink(nextChallenge?.entrypoint.orEmpty(), "Continue").build()
         } else {
             getPage().addLink(COOKIE_YOUTUBE, "Watch me", true).addElement("We need at least $COOKIE_COUNT").build()
